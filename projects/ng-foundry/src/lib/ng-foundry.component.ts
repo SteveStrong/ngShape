a5dcee5cdd93fe539e2ed34be79c471028ae46e4
Steve Strong <sstrong@apprenticesystems.com>
2018-05-07 10:05:06 -0400

    now with a lib

---
+++ projects/ng-foundry/src/lib/ng-foundry.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngFoundry',
+  template: `
+    <p>
+      ng-foundry works!
+    </p>
+  `,
+  styles: []
+})
+export class NgFoundryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
